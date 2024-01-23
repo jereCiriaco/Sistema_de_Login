5dfd50812cdaf1019b0df1c7e4de499203ed1efb
Jeremias <jeremiasciriacojc@gmail.com>
2024-01-23 13:30:30 -0300

    beta

--- prim.py
+++ prim.py
@@ -1,14 +1,13 @@
 import os
+from PySimpleGUI import PySimpleGUI as sg
 
-def validador_login(nickname, senha):
+
+
+def verifica_login(nickname, senha):
     if(nickname == "" or senha == ""):
         print("Preencha todos os camapos")
         return 0
-    else:
-        print("Campos preenchidos")
-        return 1
 
-def verifica_login(nickname):
     # Abre o arquivo em modo de leitura ('r')
     with open("banco_de_dados.txt", "r") as arquivo:
         # Lê cada linha do arquivo
@@ -25,15 +24,28 @@ def verifica_login(nickname):
 
 
 os.system("cls")
-print("Bem Vindo")
-print("--------------------------")
-nickname = input("Informe seu nickname: ")
-senha = input("Infome a senha: ")
-    
 
-validador_login(nickname, senha)
-ususario = verifica_login(nickname)
-if(ususario):
-    for info in ususario:
-        print(info, end=" ")
+#Layout 
+sg.theme('Reddit')
+layout = [
+    [sg.Text("Usuario:"), sg.Input(key = "usuario", size=(25,1))],
+    [sg.Text("Senha:  "), sg.Input(key="senha",size=(25,1),password_char="*")],
+    [sg.Button("ENTER")], 
+    [sg.Output(size=(50,5), key="output")]
+
+]
+#Janela
+janela = sg.Window("Tela de Login", layout)
+
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == "ENTER":
+        info = verifica_login(valores["usuario"],valores["senha"])
+        if(info !=None):
+            janela["output"].update(value=info)
+
+        
 
